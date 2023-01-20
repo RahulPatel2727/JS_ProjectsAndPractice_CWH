d9446d9bfd2b89056696806d29b61b32c4f8cc59
RahulPatel2727 <rahul23131420@gmail.com>
2023-01-21 01:17:21 +0530

    new files

--- js.js
+++ js.js
@@ -1,15 +1,4 @@
-let container = document.querySelectorAll("#id")[0]
-container.setAttribute("class","container")
-let button = document.querySelectorAll("#btn")[0]
-button.setAttribute("class", "color button")
-let a = document.getElementsByClassName("cont")
-Array.from(a).forEach(element => {
-    element.onclick = () =>{
-        console.log("onclick")
-        console.log("1")
-    }
-}); 
-let b = document.getElementById("btn")
-b.onclick =()=>{
-    console.log("onclick using id")
+var a=document.getElementsByClassName("cont")
+for(i of a) i.onclick=()=>{
+    alert("hooray")
 }
---
+++ push.sh
@@ -0,0 +1,6 @@
+git branch
+git status
+git add .
+git status 
+git commit -m "new files"
+git push
